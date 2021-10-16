c192a344cf84de0e9ffc26e61f1759a12119a9f0
unknown <omarmneimneh2000@gmail.com>
2021-10-16 00:51:06 -0400

    added the full game in javascript

--- gameRules.js
+++ gameRules.js
@@ -1,5 +1,43 @@
 function computerPlay(){
     let min = 1;
     let max = 3;
-    console.log(Math.floor(Math.random()*(max-min+1)+min));
+    let number = Math.floor(Math.random()*(3-1+1)+1);
+    let play = "";
+    if(number == 1)play = "rock";
+    if(number == 2)play = "paper";
+    if(number == 3)play = "scissors";
+    return play;
 }
+
+function player(){
+    let play = window.prompt("Rock, paper, or scissors?")
+}
+
+function winner(cPlay, pPlay){
+    let winner = "";
+    if(cPlay === "rock" && pPLay === "scissors") winner = "computer";
+    if(cPlay === "scissors" && pPLay === "paper") winner = "computer";
+    if(cPlay === "paper" && pPLay === "rock") winner = "computer";
+    if(cPlay === "rock" && pPLay === "paper") winner = "player";
+    if(cPlay === "paper" && pPLay === "scissors") winner = "player";
+    if(cPlay === "scissors" && pPLay === "rock") winner = "player";
+    
+    return winner;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let playerPlay = "";
+let compPlay = "";
+
+
+while((playerScore!=3)||(computerScore!=3)){
+    compPlay = computerPlay();
+    playerPlay = player();
+    
+    if(winner(compPlay, playerPlay)==="computer")computerScore++;
+    if(winner(compPlay, playerPlay)==="player")playerScore++;
+    if(computerScore==3)console.log("The computer wins :(");
+    if(playerScore==3)console.log("You win!");
+}
+
